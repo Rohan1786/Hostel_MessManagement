2810596f85bda23b36fa45da81e41c867e53481c
Rohan <rohanpawar@gmail.com>
2024-02-02 23:51:18 +0530

    admin added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ const path = require("path")
 const app = express()
 // const hbs = require("hbs")
 const LogInCollection = require("./mongo")
+const { name } = require("ejs")
+const { error } = require("console")
 const port = process.env.PORT || 3000
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
 
+
+
+
 const tempelatePath = path.join(__dirname, '../views')
 const publicPath = path.join(__dirname, '../public')
 console.log(publicPath);
 
-app.set('view engine', 'ejs')
+app.set('view engine','ejs');
+
+
 app.set('views', tempelatePath)
 app.use(express.static(publicPath))
 
@@ -24,14 +31,22 @@ app.get('/signup', (req, res) => {
     res.render('signup')
 })
 app.get('/', (req, res) => {
+    res.render('choose')
+})
+
+app.get('/login',(req,res)=>{
     res.render('login')
 })
 
-
-
 app.get('/home', (req, res) => {
     res.render('home')
-})
+});
+app.get('/admin', (req, res) => {
+    res.render('admin')
+});
+
+
+
 
 app.post('/signup', async (req, res) => {
     
@@ -68,6 +83,7 @@ app.post('/signup', async (req, res) => {
 })
 
 
+
 app.post('/login', async (req, res) => {
 
     try {
@@ -94,6 +110,40 @@ app.post('/login', async (req, res) => {
 
 
 })
+
+
+// app.post('/admin', (req, res) => {
+
+//     try {
+        
+//         // const check ={ name: "rohan" }
+
+//         // if (check.password === "123") {
+//         //     res.status(201).render("home", { naming: `${'123'}+${name}` })
+//         // }
+//         const name=document.querySelector('#name');
+//         const pass=document.querySelector('#pass');
+//         if(name=="rohan"&&pass=="1234"){
+//             res.send("hello")
+//         }
+
+//         else {
+//             res.send("incorrect password")
+            
+//         }
+
+
+//     }
+    
+//     catch (e) {
+
+//         res.render("wrong details");
+        
+
+//     }
+
+
+// })
 
 
 
